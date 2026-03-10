6682a2b50da92317b44d21430e0972debcf5bc0c
jimenavargas393-ship-it <jimenavargas393@gmail.com>
2026-03-10 17:49:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,147 +1,35 @@
-body{
-font-family: 'Segoe UI', sans-serif;
-background:#f7f5f2;
-margin:0;
-padding:30px;
-color:#333;
-}
+function exportarExcel(){
 
-h1{
-text-align:center;
-color:#5a4b42;
-}
+let tabla = `
+<table>
+<tr>
+<th>Ganador</th>
+<th>Perdedor</th>
+</tr>
+`
 
-.subtitle{
-text-align:center;
-margin-bottom:30px;
-color:#8a7f78;
-}
+votos.forEach(v=>{
+tabla += `
+<tr>
+<td>${v.ganador}</td>
+<td>${v.perdedor}</td>
+</tr>
+`
+})
 
-.container{
-display:flex;
-justify-content:center;
-gap:25px;
-}
+tabla += `</table>`
 
-.card{
-background:white;
-padding:25px;
-border-radius:12px;
-width:300px;
-box-shadow:0 8px 20px rgba(0,0,0,0.08);
-}
+let blob = new Blob([tabla], {
+type:"application/vnd.ms-excel"
+})
 
-h2{
-margin-top:0;
-color:#5a4b42;
-}
+let url = URL.createObjectURL(blob)
 
-label{
-display:block;
-margin-top:12px;
-font-weight:500;
-}
+let a = document.createElement("a")
 
-select{
-width:100%;
-padding:10px;
-margin-top:6px;
-border-radius:6px;
-border:1px solid #ddd;
-}
+a.href = url
+a.download = "votos_postres.xls"
 
-.buttons{
-display:flex;
-gap:10px;
-margin-top:15px;
-}
+a.click()
 
-button{
-border:none;
-padding:10px;
-border-radius:6px;
-cursor:pointer;
-font-weight:500;
-}
-
-.btn-blue{
-background:#a6c8ff;
-}
-
-.btn-red{
-background:#ffb4a2;
-}
-
-.tip{
-font-size:13px;
-margin-top:10px;
-color:#777;
-}
-
-.question{
-background:#f1f1f1;
-padding:10px;
-border-radius:8px;
-text-align:center;
-margin-bottom:15px;
-}
-
-.duelo{
-display:flex;
-gap:15px;
-}
-
-.postre{
-flex:1;
-background:#fff7f0;
-border:1px solid #ffe2cc;
-padding:18px;
-border-radius:10px;
-font-size:16px;
-}
-
-.postre:hover{
-background:#ffe9d6;
-}
-
-.tag{
-background:#ff9f43;
-color:white;
-padding:4px 8px;
-border-radius:5px;
-margin-right:5px;
-font-size:14px;
-}
-
-.info{
-font-size:13px;
-text-align:center;
-margin-top:10px;
-color:#777;
-}
-
-.btn-export{
-background:#bde0fe;
-width:100%;
-margin-top:10px;
-}
-
-.btn-top{
-background:#ffd166;
-width:100%;
-margin-top:10px;
-}
-
-ol{
-padding-left:20px;
-}
-
-li{
-margin-bottom:8px;
-}
-
-.ranking-info{
-font-size:12px;
-color:#777;
-margin-top:10px;
 }
